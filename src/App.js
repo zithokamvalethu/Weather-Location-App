e2b913195583d7af40dab2b6e3f96a5fcb8222c3
zithokamvalethu <kamvalethu.zitho1705@gmail.com>
2022-03-08 12:46:07 +0200

    added multiple input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import { fetchWeather } from "./components/fetchWeather";
 
-function App() {
+const WeatherApp = () => {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  // const [errorMessage, setErrorMessage] = useState();
+
+  // const dateBuild = (d) => {
+  //   let date = String(new window.Date());
+  //   date = date.slice(3, 15);
+  //   return date;
+  // };
+
+  // const handleClick = () => {
+  //   setErrorMessage("Please enter the correct city name!");
+  // };
+
+  const search = async (event) => {
+    if (event.key === "Enter") {
+      const data = await fetchWeather(query);
+
+      setWeather(data);
+      setQuery("");
+    }
+    // else {
+    //   const err = handleClick();
+    // }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <input
+        type="text"
+        className="search"
+        placeholder="Enter the city name in here"
+        value={query}
+        onChange={(event) => setQuery(event.target.value)}
+        onKeyPress={search}
+      />
+      {/* {errorMessage && <div className="error"> {errorMessage} </div>} */}
+      {weather.main && (
+        <div className="city">
+          <div className="date"> {new Date().toDateString("en-ZA ")}</div>
+          {/* <div className="date"> {new Date().toDateString("en-us")}</div> */}
+
+          <h2 className="city-name">
+            <span>{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+          </h2>
+          <div className="city-temp">
+            {Math.round(weather.main.temp)}
+            <sup>&deg;C</sup>
+          </div>
+
+          <h2 className="city-name">
+            <span>{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+          </h2>
+          <div className="city-temp">
+            {Math.round(weather.main.temp)}
+            <sup>&deg;C</sup>
+          </div>
+          <div className="info">
+            <img
+              className="city-icon"
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt={weather.weather[0].description}
+            />
+            <p>{weather.weather[0].description}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+export default WeatherApp;
